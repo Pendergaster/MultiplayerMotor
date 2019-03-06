0b45be7ee75d9bf994db678deded3d56a436926c
Broms <patrikbroms@kamk.fi>
2019-03-06 18:22:52 +0200

    stb image and inputs

--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,57 @@
 #include <glad/src/glad.c>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#define STB_IMAGE_IMPLEMENTATION
-
-//TODO(pate) add stbimage.h
-#undef STB_IMAGE_IMPLEMENTATION
 #define SCREENWIDHT 800
 #define SCREENHEIGHT 800
-int main(int argc,char* argv[])
+
+#include "inputs.h"
+
+
+static void glfw_error_callback(int e, const char *d)
 {
-	(void)argc;(void)argv;
+	printf("Error %d: %s\n", e, d);
+}
+
+GLFWwindow* init_window() 
+{
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	glfwWindowHint(GLFW_SAMPLES, 4);
-
 	glfwSwapInterval(0);
-
 	GLFWwindow* window = glfwCreateWindow(SCREENWIDHT,SCREENHEIGHT, "Tabula Rasa", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 	glViewport(0, 0, SCREENWIDHT, SCREENHEIGHT);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetErrorCallback(glfw_error_callback);
+	return window;
+}
 
-	while (!glfwWindowShouldClose(window))
-	{
 
+int main(int argc,char* argv[])
+{
+	
+	(void)argc;(void)argv;
+	GLFWwindow* window = init_window();
+	Input inputs;
+	init_inputs(&inputs);
+	glfwSetWindowUserPointer(window,&inputs);
+
+	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
-		printf("update!\n");
+		glm::vec2 mpos = get_mouse_position();
+		printf("update! , %f ,%f ",mpos.x,mpos.y);
+		if(key_pressed(Key::KEY_E)) {
+			LOG("e pressed");
+		} else if (key_down(Key::KEY_E)) {
+			LOG("e down\n");
+		}
+
+		update_keys(&inputs);
 		int display_w,display_h;
 		glfwGetFramebufferSize(window, &display_w, &display_h);
 		glViewport(0, 0, display_w, display_h);
---
+++ src/utils.h
@@ -0,0 +1,50 @@
+#ifndef MP_UTILS
+#define MP_UTILS
+#include <stdint.h>
+#include <stdio.h>
+
+typedef uint64_t	u64;
+typedef uint32_t	u32;
+typedef int32_t		i32;
+typedef uint16_t	u16;
+typedef uint8_t		u8;
+static const u16 numeric_max_u16 = 0xFFFF;
+static const u32 numeric_max_u32 = 0xFFFFFFFF;
+static const u64 numeric_max_u64 = 0xFFFFFFFFFFFFFFFF;
+
+#define LOG_STR "LOG: "
+#define LOG(...) do{printf(LOG_STR __VA_ARGS__ ); \
+printf(" in file : %s:%d \n", __FILE__, __LINE__);} while(0) 
+
+#define ASSERT_MESSAGE(condition,...) do{ if(!(condition)){  LOG(__VA_ARGS__); assert(condition);}}while(0) 
+#define ABORT_MESSAGE(...) do{ LOG(__VA_ARGS__);fflush(stdout); assert(false); }while(0) 
+
+#define BIT_CHECK(a,b) ((a & b) > 0) // onks oikein?
+#define BIT_SET(a,b) ( a |= b)
+#define BIT_UNSET(a,b) (a &= ~b)
+
+#define KILOS(NUM) (NUM * 1000)
+#define MEGAS(NUM) (NUM * 1000000)
+#define GIGAS(NUM) (NUM * 1000000000)
+
+#define BETWEEN(a,x,b) (a < x && b > x)
+
+//https://pastebin.com/3YvWQa5c
+template<typename T>
+struct CleanUP {
+    T lambda;
+    CleanUP(T lambda):lambda(lambda){}
+    ~CleanUP(){lambda();}
+    CleanUP(const CleanUP&);
+  private:
+    CleanUP& operator =(const CleanUP&);
+};
+
+class CleanUpHelp {
+  public:
+    template<typename T>
+        CleanUP<T> operator+(T t){ return t;}
+};
+
+#define defer const auto& CONCAT(defer__, __LINE__) = CleanUpHelp() + [&]()
+#endif //MP_UTILS
