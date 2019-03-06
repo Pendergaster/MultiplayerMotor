ad1b29964883986129355de692402ed36019c1c6
Broms <patrikbroms@kamk.fi>
2019-03-06 16:44:40 +0200

    start point

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,6 @@
+def FlagsForFile(filename, **kwargs ):
+  return {
+    'flags': [ '-x','c++' ,'-Wall', '-Wextra', '-Werror' ,'-I./external/']
+  }
+    
+# ,'-IC:/Users/Pate/Desktop/PakkiUtils']
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define NOMINMAX
+#include <stdio.h>
+#include <stdlib.h>
+#include <glad/include/glad/glad.h>
+#include <glad/src/glad.c>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+
+//TODO(pate) add stbimage.h
+#undef STB_IMAGE_IMPLEMENTATION
+#define SCREENWIDHT 800
+#define SCREENHEIGHT 800
+int main(int argc,char* argv[])
+{
+	(void)argc;(void)argv;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+
+	glfwSwapInterval(0);
+
+	GLFWwindow* window = glfwCreateWindow(SCREENWIDHT,SCREENHEIGHT, "Tabula Rasa", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	glViewport(0, 0, SCREENWIDHT, SCREENHEIGHT);
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+		glfwPollEvents();
+		printf("update!\n");
+		int display_w,display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
+	printf("bye!\n");
+	return 0;
+}
