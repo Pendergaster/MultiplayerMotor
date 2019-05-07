0032481a79b5a3118a7c4c718d95a48a7a5950d0
RetzKu <mikakaunisto@kamk.fi>
2019-05-07 14:17:46 +0300

    Pelaaja ei vedä voltteja

--- src/server/objecttype.h
+++ src/server/objecttype.h
@@ -12,9 +12,14 @@ enum class ObjectType : int {
 struct ObjectTracker {
 	ObjectType	type;
 	vec3		pos;
-	vec3		velocity;
-	vec3		angularVelocity;
 	quaternion	orientation;
+};
+
+struct PlayerData {
+	char	name[12];
+	int		score;
+	int		kuutioId;
+	int		playerId;
 };
 
 const vec3 floor_scale(10,1,10);
@@ -23,9 +28,8 @@ const vec3 player_scale(1,1,1);
 const vec3 physics_gravity(0,-7,0);
 const float floor_friction = 0.1f;
 const float free_mass = 1.f;
+
 //kun objecti ei ole enään aktiivinen ID menee menee id INACTIIVINEN -> clientti poistaa simulaation
-
 //kun id on erisuuri kuin clientillä spawnataan uusi objecti
-
 //clientti lähettää pelkän input staten
 #endif
